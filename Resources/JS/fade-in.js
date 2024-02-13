d194e9e081adec892759faf8c3eac691bf4d729d
0xBrian <brian@draftlabs.app>
2024-02-13 14:06:17 -0500

    Fade bg images 4 boxes
    Added image fade on hover for 4 boxes

--- Resources/JS/fade-in.js
+++ Resources/JS/fade-in.js
@@ -1,9 +1,38 @@
+/* FADE EVENT LISTENERS ----------------------------------------------------------*/ */
 document.addEventListener('DOMContentLoaded', function() {
+    /* PROJECT LISTENERS */
     var projectBox = document.getElementById('projects');
     projectBox.addEventListener('mouseover', function() {
         this.querySelector('.project-bg').style.opacity = 1;
     });
     projectBox.addEventListener('mouseout', function() {
         this.querySelector('.project-bg').style.opacity = 0;
+    });
+
+    /* PROJECT LISTENERS */
+    var projectBox = document.getElementById('about');
+    projectBox.addEventListener('mouseover', function() {
+        this.querySelector('.about-bg').style.opacity = 1;
+    });
+    projectBox.addEventListener('mouseout', function() {
+        this.querySelector('.about-bg').style.opacity = 0;
+    });
+
+    /* PROJECT LISTENERS */
+    var projectBox = document.getElementById('contact');
+    projectBox.addEventListener('mouseover', function() {
+        this.querySelector('.contact-bg').style.opacity = 1;
+    });
+    projectBox.addEventListener('mouseout', function() {
+        this.querySelector('.contact-bg').style.opacity = 0;
+    });
+
+    /* PROJECT LISTENERS */
+    var projectBox = document.getElementById('follow');
+    projectBox.addEventListener('mouseover', function() {
+        this.querySelector('.follow-bg').style.opacity = 1;
+    });
+    projectBox.addEventListener('mouseout', function() {
+        this.querySelector('.follow-bg').style.opacity = 0;
     });
 });
