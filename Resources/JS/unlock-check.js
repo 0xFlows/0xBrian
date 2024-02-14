cab9191d498778e86a1715a51ea6f820df1f3a4f
0xBrian <brian@draftlabs.app>
2024-02-14 15:38:58 -0500

    Sepolia switch, SC added

--- Resources/JS/unlock-check.js
+++ Resources/JS/unlock-check.js
@@ -2,6 +2,9 @@ function unlock() {
     document.getElementById('unlock-label').innerHTML = "unlocked!";
 }
 
+const submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', unlockCheck);
 function unlockCheck(string) {
     if(string === "Hiro") {
         unlock();
